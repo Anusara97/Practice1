ca3398dbeab16e0e5196c780d7ad816964934e60
Anusara97 <anusara777rcm@gmail.com>
2024-06-28 15:08:28 +0530

    Test.java file is updated (3)

--- Test.java
+++ Test.java
@@ -18,13 +18,27 @@ public class Test {
         Member.calTotal(m);
         System.out.println("The total amount of each members: " +Member.getTotal() +"\n");
 
-        System.out.println("Enter a ID: ");
+        System.out.print("Enter a ID: ");
         id = in.nextInt();
         display(m, id);
         in.nextLine();
-        System.out.println("Enter a name: ");
+
+        System.out.print("Enter a name: ");
         name = in.nextLine();
         display(m, name);
+
+        //change the id of 1st member
+        m[0].setId(6585);
+
+        //change the name of the last member
+        m[5].setName("Nuwan");
+
+        //add another 3000 amount to the 3rd member
+        m[2].addAmount(3000);
+
+        Member.calTotal(m);
+        System.out.println("The total amount of each members: " +Member.getTotal() +"\n");
+
     }
 
     public static void display(Member [] m, int id) {
@@ -43,15 +57,19 @@ public class Test {
 
     public static void display(Member [] m, String name) {
         boolean status = false;
+        int count = 0;
         for (int i=0; i<m.length; i++) {
             if (name.equals(m[i].getName()) ) {
                 System.out.println("ID: " +m[i].getId() +"\nName: " +m[i].getName() + "\nAmount: " + m[i].getAmount() +"\n");
                 status = true;
-                break;
+                count++;
             }
         }
         if (status == false) {
             System.out.println(name +" is not a member.");
+        }
+        if (count > 1) {
+            System.out.println("No. of members with the name " +name + " is :" +count);
         }
     }
 }
